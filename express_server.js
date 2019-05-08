6ce02afb802ada5221b8880de712e3b835892394
jitammy <jitammy@gmail.com>
2019-05-08 00:18:30 +0000

    implement a DELETE operation to remove existing shortened URLs database

--- express_server.js
+++ express_server.js
@@ -1,9 +1,13 @@
+// require npm package to use
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
-app.set("view engine", "ejs")
 const bodyParser = require("body-parser");
+var morgan = require('morgan');
+// app use
+app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('short'));
 // functions
 function generateRandomString(){
     let random_string = '';
@@ -19,30 +23,29 @@ var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+// get 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
-  });
+});
 //   res.json([body]) 发送一个json的响应。这个方法和将一个对象或者一个数组作为参数传递给res.send()方法的效果相同
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
-app.get("/urls", (req, res) => {
-let templateVars = { 
-    urls: urlDatabase 
-    };
-res.render("urls_index", templateVars);
 });
-// get new url from website
+//  get home page
+app.get("/urls", (req, res) => {
+    let templateVars = { 
+        urls: urlDatabase 
+    };
+    res.render("urls_index", templateVars);
+});
+// get create new url page
 app.get("/urls/new", (req, res) => {
     res.render("urls_new");
-  });
-// show shortURL
+});
+// get reading shortURL 
 app.get("/urls/:shortURL", (req, res) => {
     // console.log("hello")
     let templateVars = {
@@ -50,20 +53,27 @@ app.get("/urls/:shortURL", (req, res) => {
         longURL: urlDatabase[req.params.shortURL] 
     };
     res.render("urls_show", templateVars);
-  });
-  //post 
-  app.post("/urls", (req, res) => {
-    console.log(req.body.longURL);  // Log the POST request body to the console
+});
+//post create a new ShortURL
+app.post("/urls", (req, res) => {
+    console.log(`longURL is ${req.body.longURL}`);  // Log the POST request body to the console
     // res.send("Ok");         // Respond with 'Ok' (we will replace this)
     let randomString = generateRandomString()
     urlDatabase[randomString] = req.body.longURL;
     console.log(urlDatabase)
     res.status(200)
-
     res.redirect(`/urls/${randomString}`)
-  });
-
-  app.get("/u/:shortURL", (req, res) => {
+});
+app.get("/u/:shortURL", (req, res) => {
     const longURL = urlDatabase[req.params.shortURL]
     res.redirect(longURL);
-  });
+});
+// post: delete
+app.post('/urls/:shortURL/delete', (req, res) => {
+    delete urlDatabase[req.params.shortURL]
+    res.redirect('/urls')
+})
+// listen on port 8080 for http requst
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+});
