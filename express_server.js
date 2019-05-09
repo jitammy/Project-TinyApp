edb8661bfc4c9ebdbcdc3c1657cb4b9d725437ad
jitammy <jitammy@gmail.com>
2019-05-09 17:16:15 +0000

    register & login working sofar...

--- express_server.js
+++ express_server.js
@@ -27,21 +27,27 @@ function generateRandomString(){
     }
     return random_string
 }
-function getUserById (user_id){
-    if(user_id = req.session.user_id){
-        let  user = users[user_id]
+// iterate users database to check if user email exist to decide whether register the user
+function getUserByEmail(email){
+    for(user in users){
+        if(user.email === email){
+            return user
+        }
     }
-    return user
-}
-function getUserByEmail(){
+    return false;
 }
 // database
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
-const users = {}
-
+const users = { 
+    "userRandomID": {
+      id: "userRandomID", 
+      email: "user@example.com", 
+      password: "purple-monkey-dinosaur"
+    },
+}
 // get 
 app.get("/", (req, res) => {
 //   res.send("Hello!");
@@ -71,7 +77,8 @@ app.get("/urls", (req, res) => {
 // get create new url page
 app.get("/urls/new", (req, res) => {
     let templateVars ={
-        user: user
+        user: users[req.session.user_id],
+
     }
     console.log(templateVars)
     res.render("urls_new", templateVars);
@@ -82,7 +89,8 @@ app.get("/urls/:shortURL", (req, res) => {
     let templateVars = {
         shortURL: req.params.shortURL,
         longURL: urlDatabase[req.params.shortURL],
-        user: user
+        user: users[req.session.user_id],
+
     };
     res.render("urls_show", templateVars);
 });
@@ -94,7 +102,11 @@ app.post("/urls", (req, res) => {
     urlDatabase[randomString] = req.body.longURL;
     console.log(urlDatabase)
     res.status(200)
-    res.redirect(`/urls/${randomString}`)
+    let templateVars = {
+        user: users[req.session.user_id],
+
+    }
+    res.redirect(`/urls/${randomString}`, templateVars)
 });
 app.get("/u/:shortURL", (req, res) => {
     const longURL = urlDatabase[req.params.shortURL]
@@ -102,6 +114,10 @@ app.get("/u/:shortURL", (req, res) => {
 });
 app.get("/register", (req, res)=>{
     res.render("register")
+})
+app.get("/login", (req, res)=>{
+
+    res.render("login", templateVars)
 })
 // post: delete
 app.post('/urls/:shortURL/delete', (req, res) => {
@@ -115,69 +131,64 @@ app.post('/urls/:shortURL', (req, res)=>{
     console.log(req.params.shortURL)
     res.redirect('/urls')
 }) 
-// app.post('/login',(req, res)=> {
-//     // req.session('username', req.body.username)
-//     // res.redirect('/urls')
-//     const {email, password}= req.body
-//     if (email && password){
-//         const user = getUserById()
-//         if (user){
-//             bcrypt.compare(password, user.password, (err, result)=>{
-//                 if(err){
-//                     console.log(" Error checking passwords", err)
-//                     res.redirect('/login')
-//                 } else if (result){
-//                     users[randomUserID] = {
-//                         user_id: randomUserID,
-//                         email: email,
-//                         password: password
-//                     }
-//                     req.session.user_id = user_id
-//                     res.redirect('/')
-//                 } else {
-//                     console.log('Pls login')
-//                     res.redirect('/login')
-//                 }
-//             })
-//         } else {
-//             console.log('Invalid username or password')
-//             res.redirect('/login')
-//         }
-//     }
-// })
+app.post('/login',(req, res)=> {
+    // req.session('username', req.body.username)
+    // res.redirect('/urls')
+    const {email, password}= req.body
+    if (email && password) {
+        if (getUserByEmail(email)){
+            bcrypt.compare(password, user.password, (err, result)=>{
+                if(err){
+                    console.log(" Error checking passwords", err)
+                    res.redirect('/login')
+                } else if (result){
+                    req.session.user_id = user_id
+                    res.redirect('/urls')
+                } else {
+                    res.send(403, `Password in valid`)
+                }
+            })
+        } else {
+            res.send(403, `Invalid user or password`)
+        }
+    } else {
+        res.send(400, `Pls provide valid user information`)
+    }
+})
 app.post('/logout', (req,res)=>{
-    // res.clearCookie('username')
-    req.session = null
+    req.session.user_id = null
     res.redirect('/urls')
 })
 app.post("/register", (req, res)=>{
     let randomUserID = generateRandomString()
     const {email, password, password_confirm}= req.body
     console.log(req.body)
-    if ( email && password && password_confirm){
-        if(password === password_confirm){
-            bcrypt.hash(password, 10 ,(err, password_hashed)=>{
-                if (err){
-                    console.log("Got an error hashing the password")
-                    res.redirect("/register")
-                } else {
-                    users[randomUserID] = {
-                        user_id: randomUserID,
-                        email: email,
-                        password: password_hashed
-                    }
+    if(getUserByEmail(email)){
+        res.send(400, `<h2>"BAD REQUEST USER ALREADY EXIST"</h2>`)
+    } else {
+        if ( email && password && password_confirm){
+            if(password === password_confirm){
+                bcrypt.hash(password, 10 ,(err, password_hashed)=>{
+                    if (err){
+                        console.log("Got an error hashing the password")
+                        res.redirect("/register")
+                    } else {
+                        users[randomUserID] = {
+                            user_id: randomUserID,
+                            email: email,
+                            password: password_hashed
+                        }
                     req.session.user_id = randomUserID
                     res.redirect("/urls")
-                }
-            })
-        } else {
-            console.log("password not match")
-            res.redirect ("/register")
-        }
-    } else if(email){
-        res.send(400, "BAD REQUEST PLSESE INPUT REGISTRATION INFO")
-    } else {
-        res.send(400, "BAD REQUEST USER ALREADY EXIST")
+                    }
+                })
+            } else {
+                console.log("password not match")
+                res.redirect ("/register")
+            }
+        } else if(!email || !password || !password_confirm){
+            res.send(400, `<h2>BAD REQUEST PLSESE INPUT REGISTRATION INFO</h2>`)
+        } 
     }
 })
 // listen on port 8080 for http requst
